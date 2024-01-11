6b9902b60b8717df607e3c0bedd77d06ddc53b2a
tutusebooi <155731156+tutusebooi@users.noreply.github.com>
2024-01-11 10:13:56 +0000

    Thu, Jan 11, 2024, 12:13 PM +02:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,7 +1,7 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = (dollars * percent
+    tip = int(dollars) * (percent)
     print(f"Leave ${tip:.2f}")
 
 
