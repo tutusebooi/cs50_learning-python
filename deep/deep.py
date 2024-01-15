5d535300769ab19e358ad450e75cf8c8fb9c7115
tutusebooi <155731156+tutusebooi@users.noreply.github.com>
2024-01-15 11:52:18 +0000

    Mon, Jan 15, 2024, 1:52 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,7 @@
 
 x = input('what is the answer to the Great Question of Life, the Universe, and Everything? ')
 y = x.strip()
-if y == "42" or y == "forty-two" or y == "forty two":
+if y == "42" or y == "forty-two" or y == "forty two".lower():
     print("Yes")
 else:
     print("No")
