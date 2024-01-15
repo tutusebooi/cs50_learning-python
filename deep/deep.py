d15b9eab075fe8f67ac373c7eca5b48aa8ae4b5e
tutusebooi <155731156+tutusebooi@users.noreply.github.com>
2024-01-15 11:36:31 +0000

    Mon, Jan 15, 2024, 1:36 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,2 +1,7 @@
 
-x = input('')
+x = input('what is the answer to the Great Question of Life, the Universe and Everything? ').lower()
+
+if x == "yes" or x == "forty-two" or x == "forty two":
+    print("Yes)
+else:
+    print("No)
