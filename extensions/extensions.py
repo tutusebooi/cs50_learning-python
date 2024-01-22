43e1b0c7f65411cb628294ec02dc7bcf9ca47ee6
tutusebooi <155731156+tutusebooi@users.noreply.github.com>
2024-01-22 09:42:27 +0000

    Mon, Jan 22, 2024, 11:42 AM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,3 +3,9 @@ if user.endswith("gif"):
     print("image/gif")
 if user.endswith("jpg"):
     print("image/jpg")
+if user.endswith("jpeg"):
+    print("image/jpeg")
+elif user.endswith("png"):
+    print("image/png")
+elif user.endswith(""):
+    print("")
