96813ac3c98af89fc3876b205f005caa36d5dd14
tutusebooi <155731156+tutusebooi@users.noreply.github.com>
2024-01-22 09:44:52 +0000

    Mon, Jan 22, 2024, 11:44 AM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,11 +1,11 @@
-user = input("File name: ")
+user = input("File name: ").strip().lower()
 if user.endswith("gif"):
     print("image/gif")
-if user.endswith("jpg"):
+elif user.endswith("jpg"):
     print("image/jpg")
 if user.endswith("jpeg"):
     print("image/jpeg")
 elif user.endswith("png"):
     print("image/png")
-elif user.endswith(""):
-    print("")
+elif user.endswith("pdf"):
+    print("application/pdf")
