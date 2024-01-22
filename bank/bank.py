0f91ed23cc00204692708f07d421428a72f92d09
tutusebooi <155731156+tutusebooi@users.noreply.github.com>
2024-01-22 08:20:25 +0000

    Mon, Jan 22, 2024, 10:20 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -5,6 +5,7 @@ if greeting.startswith("hello"):
     print("$0")
 elif greeting == "How you doing?":
     print("$20")
+elif greeting == "What's happening?":
+    print("$100")
 else:
     print("$100")
-
