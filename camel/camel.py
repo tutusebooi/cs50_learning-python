8c4f8d052974df37fd16737582857c6f8a23cb9b
tutusebooi <155731156+tutusebooi@users.noreply.github.com>
2024-02-07 13:49:49 +0000

    Wed, Feb 7, 2024, 3:49 PM +02:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,8 +1,16 @@
+def camel_to_snake(variable_name):
+    snake_case_name = ''
+    for char in variable_name:
+        if char.isupper():
+            snake_case_name += '_' + char.lower()
+        else:
+            snake_case_name += char
+    return snake_case_name.lstrip('_')
 
-def came_name():
-    n = ''
-    for letter in n:
-        if letter.count(n) =< 4:
-            return letter
-        if letter 
+def main():
+    camel_case_name = input("Enter a variable name in camel case: ")
+    snake_case_name = camel_to_snake(camel_case_name)
+    print("Snake case name:", snake_case_name)
 
+if __name__ == "__main__":
+    main()
